248466792cb720ccf9197e39489be72c967cfe63
Елизавета <lizalizapotter123@gmail.com>
2024-11-21 03:04:35 +0300

    Добавьте файлы проекта.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Lab3.UsersClasses
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxSubject = new System.Windows.Forms.TextBox();
+            this.textBoxBody = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(332, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Отправление писем по email";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(53, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Кому";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(53, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Текст";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(53, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(522, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Имя";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(53, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Заголовок";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(165, 124);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(258, 22);
+            this.textBoxEmail.TabIndex = 6;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(632, 124);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(258, 22);
+            this.textBoxName.TabIndex = 7;
+            // 
+            // textBoxSubject
+            // 
+            this.textBoxSubject.Location = new System.Drawing.Point(167, 196);
+            this.textBoxSubject.Name = "textBoxSubject";
+            this.textBoxSubject.Size = new System.Drawing.Size(723, 22);
+            this.textBoxSubject.TabIndex = 8;
+            // 
+            // textBoxBody
+            // 
+            this.textBoxBody.Location = new System.Drawing.Point(167, 268);
+            this.textBoxBody.Multiline = true;
+            this.textBoxBody.Name = "textBoxBody";
+            this.textBoxBody.Size = new System.Drawing.Size(723, 193);
+            this.textBoxBody.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(392, 494);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 42);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 574);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxBody);
+            this.Controls.Add(this.textBoxSubject);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxSubject;
+        private System.Windows.Forms.TextBox textBoxBody;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lab3.UsersClasses;
+using static Lab3.UsersClasses.InfoEmailSending;
+using WindowsFormsApp1.UserClasses;
+
+namespace Lab3.UsersClasses
+{
+    public partial class MainForm : Form
+    {
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxEmail.Text) || string.IsNullOrWhiteSpace(textBoxName.Text) || string.IsNullOrWhiteSpace(textBoxSubject.Text) || string.IsNullOrWhiteSpace(textBoxBody.Text))
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+
+            string smtp = "smtp.mail.ru";
+            StringPair fromInfo = new StringPair("kkentttt05@mail.ru", "Корчагина Елизавета");
+            string password = "ihhmphnyQk0LJjaabgvw";
+            StringPair toInfo = new StringPair(textBoxEmail.Text, textBoxName.Text);
+            string subject = textBoxSubject.Text; string body = $"{DateTime.Now} \n" +
+            $"{Dns.GetHostName()} \n" +
+            $"{Dns.GetHostAddresses(Dns.GetHostName()).First()} \n" +
+            $"{textBoxBody.Text}";
+            InfoEmailSending info =
+            new InfoEmailSending(smtp, fromInfo, password, toInfo, subject, body);
+            SendingEmail sendingEmail = new SendingEmail(info); sendingEmail.Send();
+            MessageBox.Show("Письмо отправлено!");
+            foreach (TextBox textBox in Controls.OfType<TextBox>()) textBox.Text = "";
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+            textBoxEmail.Text = "task_code_development@list.ru";
+            textBoxName.Text = "Антон Игоревич";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
